4cb93ae6e0c8dfe150c9357fc6be2e1107c85aeb
Ubuntu <ubuntu@ip-172-31-19-173.ec2.internal>
2024-07-29 17:57:56 +0000

    test

--- src/rag/test.py
+++ src/rag/test.py
@@ -18,48 +18,6 @@ cached_graph = 'dataset/retrieved/optimal_subg'
 cached_desc = 'dataset/retrieved/desc'
 
 
-class CitationNetworkDataset(Dataset):
-    def __init__(self):
-        super().__init__()
-        self.prompt = 'Please answer the given question.'
-        self.graph = None
-        self.graph_type = 'Knowledge Graph'
-        dataset = datasets.load_dataset("rmanluo/RoG-webqsp")
-        self.dataset = datasets.concatenate_datasets([dataset['train'], dataset['validation'], dataset['test']])
-        self.q_embs = torch.load(f'{path}/q_embs.pt')
-
-    def __len__(self):
-        """Return the len of the dataset."""
-        return len(self.dataset)
-
-    def __getitem__(self, index):
-        data = self.dataset[index]
-        question = f'Question: {data["question"]}\nAnswer: '
-        graph = torch.load(f'{cached_graph}/{index}.pt')
-        desc = open(f'{cached_desc}/{index}.txt', 'r').read()
-        label = ('|').join(data['answer']).lower()
-
-        return {
-            'id': index,
-            'question': question,
-            'label': label,
-            'graph': graph,
-            'desc': desc,
-        }
-
-    def get_idx_split(self):
-
-        # Load the saved indices
-        with open(f'{path}/split/train_indices.txt', 'r') as file:
-            train_indices = [int(line.strip()) for line in file]
-        with open(f'{path}/split/val_indices.txt', 'r') as file:
-            val_indices = [int(line.strip()) for line in file]
-        with open(f'{path}/split/test_indices.txt', 'r') as file:
-            test_indices = [int(line.strip()) for line in file]
-
-        return {'train': train_indices, 'val': val_indices, 'test': test_indices}
-
-
 def process():
     os.makedirs(cached_desc, exist_ok=True)
     os.makedirs(cached_graph, exist_ok=True)
@@ -86,7 +44,8 @@ def process():
         print(graph.edge_attr)
         question=data['question']
         answer=data['answer']
-        q_emb = text2embedding(model, tokenizer, device, question)
+        q_emb = torch.load(f'/home/ubuntu/Sci-Retriever/dataset/qa_qemb/{index}.pt')
+        abstract_emb = text2embedding(model, tokenizer, device, abstract)
         subg=retrieval_via_pcst(graph, q_emb, topk=3, topk_e=5, cost_e=0.5)
 
         desc=BM25(question,subg,topk=3) #加了一个bm25here
@@ -98,13 +57,3 @@ def process():
 if __name__ == '__main__':
 
     process()
-
-    # dataset = CitationNetworkDataset()
-
-    # data = dataset[1]
-    # for k, v in data.items():
-    #     print(f'{k}: {v}')
-
-    # split_ids = dataset.get_idx_split()
-    # for k, v in split_ids.items():
-    #     print(f'# {k}: {len(v)}')
